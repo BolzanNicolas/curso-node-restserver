1dc869a888eac4d6c290d3bf97d2598ce6af721f
Nicolas Bolzan <nbolzan@intermediait.com>
2025-01-08 20:29:35 -0300

    Fin seccion 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const response = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const { generateJWT } = require("../helpers/generate-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -24,9 +25,9 @@ const login = async (req, res = response) => {
 
     // Verify password
     const validPassword = bcrypt.compareSync(password, user.password);
-    if(!validPassword ) {
+    if (!validPassword) {
       return res.status(400).json({
-        msg: 'User / Password are not correct - password',
+        msg: "User / Password are not correct - password",
       });
     }
 
@@ -35,9 +36,8 @@ const login = async (req, res = response) => {
 
     return res.json({
       user,
-      token
+      token,
     });
-    
   } catch (error) {
     return res.status(500).json({
       msg: "Contact the administrator",
@@ -45,6 +45,51 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, picture, email } = await googleVerify(id_token);
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      // Create user
+      const data = {
+        name,
+        email,
+        image: picture,
+        email,
+        password: ":P",
+        google: true
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    // If user is deleted
+    if(!user.state) {
+      return res.status(401).json({
+        msg: 'Please contact the administrator, user is blocked'
+      });
+    }
+
+    // Generate JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      id_token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: "Token could not be verified",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
